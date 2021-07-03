ce90cde636413c778b18a9bc44994fca4fd2a2f8
Aryan Shah <ac107214@avcol.school.nz>
2021-07-03 17:01:13 +1200

    Login and Register now working

--- ToDoListApp/Program.cs
+++ ToDoListApp/Program.cs
@@ -8,18 +8,71 @@ namespace ToDoListApp
     {
         static void Main(string[] args)
         {
-            //var arrUsers = new User[];
+            var UserArr = new Users[]
+        {
+            new Users("aryan","shah"),
+            new Users("dan","smith"),
+            new Users("cooper","coop")
+        };
 
-            Console.WriteLine("Hello, welcome to my to do list app.");
-            Console.WriteLine("You can create, view, update and delete your to do items.");
-            Console.WriteLine("Type any key to advance");
-            Console.ReadKey();
+            //Console.WriteLine("Hello, welcome to my to do list app.");
+            //Console.WriteLine("You can create, view, update and delete your to do items.");
+            //Console.WriteLine("Type any key to advance");
+            //Console.ReadKey();
+            Start: 
             Console.WriteLine("Type \"1\" for login and \"2\" to register");
             string LoR = Console.ReadLine();
 
+            bool correctLogin = false;
+            while(!correctLogin)
+            {
+                if(LoR == "1")
+                {
+                    Console.WriteLine("Please type your username");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Please type your password");
+                    string password = Console.ReadLine();
+                    foreach (Users user in UserArr)
+                    {
+                        if (username == user.username && password == user.password)
+                        {
+                            Console.WriteLine("You have successfully logged in !!!");
+                            Console.ReadLine();
+                            correctLogin = true;
+                            break;
+                        }
+                    }
 
-            Console.Clear();
-            Login.LoginSystem();
+                    if (!correctLogin)
+                    {
+                        Console.WriteLine("Your username or password is incorect, try again !!!");
+                    }
+                }
+                else if(LoR == "2")
+                {
+                    Console.WriteLine("Enter username");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("Enter password");
+                    string password = Console.ReadLine();
+            
+                Array.Resize(ref UserArr, UserArr.Length + 1);
+                UserArr[UserArr.Length - 1] = new Users(username, password);
+                correctLogin = true;
+                Console.WriteLine("You have sucessfully registered");
+
+            }
+            else
+            {
+                Console.WriteLine("Try again !!!");
+                goto Start;
+
+
+
+            }
+        }
+
+            
+            
             Console.WriteLine("Please enter a value from 'A','B','C' or 'D' ");
             string[] toDoChosen = { "A", "B", "C", "D" };
 
@@ -48,10 +101,10 @@ namespace ToDoListApp
         {
             public static void LoginSystem()
             {
-                Console.WriteLine("Please enter your username");
-                Console.WriteLine("Please enter your password");
-                string LogIn = Console.ReadLine();
-                Dictionary<string, string> UserNamePassword = new Dictionary<string, string>();
+                //Console.WriteLine("Please enter your username");
+                //Console.WriteLine("Please enter your password");
+                //string LogIn = Console.ReadLine();
+                //Dictionary<string, string> UserNamePassword = new Dictionary<string, string>();
                /* foreach (Login in string.Parse(UserNamePassword))
                 {
                     Login.Contains(i);
@@ -84,7 +137,19 @@ namespace ToDoListApp
                   string passWordInput = Console.ReadLine();
               }*/
         }
-        
-        
+
+        public class Users
+        {
+            public string username;
+            public string password;
+
+            public Users(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
+            }
+        }
+
+
     }
     }
